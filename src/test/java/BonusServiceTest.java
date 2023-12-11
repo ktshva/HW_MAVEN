d1dea8998e4c1b55e2317e928345d88890fc7ba2
Arina Kotysheva <kotysheva.arina@gmail.com>
2023-12-11 21:41:26 +0700

    last commit

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -36,13 +36,13 @@ public class BonusServiceTest {
     }
 
     @Test
-    void CalculateWithZeroAmount() {
+    void shouldCalculateForNotRegisteredAndUnderLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 0;
-        boolean registered = true;
-        long expected = 0;
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
@@ -52,13 +52,29 @@ public class BonusServiceTest {
     }
 
     @Test
-    void shouldCalculateForNotRegisteredA() {
+    void shouldCalculateForNotRegisteredAndOverLimit() {
         BonusService service = new BonusService();
 
         // подготавливаем данные:
-        long amount = 1000;
+        long amount = 1_000_000;
         boolean registered = false;
-        long expected = 10;
+        long expected = 500;
+
+        // вызываем целевой метод:
+        long actual = service.calculate(amount, registered);
+
+        // производим проверку (сравниваем ожидаемый и фактический):
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void CalculateWithZeroAmount() {
+        BonusService service = new BonusService();
+
+        // подготавливаем данные:
+        long amount = 0;
+        boolean registered = true;
+        long expected = 0;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
